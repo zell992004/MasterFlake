e6e60620596d9b9ccc5f3b521279cc4571b0fe6a
zell <zell992004@gmail.com>
2023-10-15 08:51:35 -0400

    1

--- flake.nix
+++ flake.nix
@@ -12,37 +12,47 @@
 
  outputs = inputs@{ nixpkgs, home-manager, ... }: 
   let
-  homeManagerConfFor = config:
-
-  zellmain = home-manager.lib.homeManagerConfiguration {
     system = "x86_64-linux";
-    modules = [
-      ./Hosts/G14/Configuration.nix
-      {
-        home-manager.users.zell = homeManagerConfFor ./Hosts/G14/Configurations.nix;
-       }
-      ];
-    extraSpecialArgs = { inherit nixpkgs; };
-  };
+    pkgs = nixpkgs.legacyPackages.${system}
+#  homeManagerConfFor = config:
 
-  in {
-    nixosConfigurations = nixpkgs.lib.nixosSystem {
+#  zellmain = home-manager.lib.homeManagerConfiguration {
+#    system = "x86_64-linux";
+#    modules = [
+#      ./Hosts/G14/Configuration.nix
+#      {
+#        home-manager.users.zell = homeManagerConfFor ./Hosts/G14/Configurations.nix;
+#       }
+#      ];
+#    extraSpecialArgs = { inherit nixpkgs; };
+#  };
+
+ in {
+    nixosConfigurations = {
       hyprland = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
         modules = [
           ./Hosts/P72/configuration.nix
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.zell = homeManagerConfFor ./Hosts/P72/home.nix;
+            home-manager.users.zell = ./Hosts/P72/home.nix;
 
             # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
           }
         ];
-        specialArgs = {inherit nixpkgs; };
       };
-      defaultPackage.x86_64-linux = zellmain.actionPackage;
+      zellmain = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./Hosts/G14/Configuration.nix
+            {
+              home-manager.users.zell = ./Hosts/G14/Configurations.nix;
+            }
+        ];
+    extraSpecialArgs = { inherit nixpkgs; };
+        };
+
     };
-  };
-}
+  }
+
