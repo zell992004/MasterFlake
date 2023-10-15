bb1681c4a2964479a087666d0735821f1c7b31db
zell <zell992004@gmail.com>
2023-10-15 11:17:21 -0400

    fixed path to zsh

--- Hosts/P72/configuration.nix
+++ Hosts/P72/configuration.nix
@@ -8,7 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ../../apps/zshconf.nix
+      ../../apps/common/zshconf.nix
     ];
 
   # Bootloader.
