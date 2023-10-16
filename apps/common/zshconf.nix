ed651fc27e7b89f99a9e2699cb146aa4c68fa5cd
zell992004 <zell992004@gmail.com>
2023-10-16 19:34:59 -0400

    1

--- apps/common/zshconf.nix
+++ apps/common/zshconf.nix
@@ -5,26 +5,36 @@
     enable = true;
     enableCompletion = true;
     enableAutosuggestions = true;
+    
+   #  history = {
+   #     enable = true;
+   #     expireDuplicatesFirst = true;
+   #     save = 100000000;
+   #     size = 1000000000;
+   #   };
+
+   
+   
    # enableVteIntegrations.enable = true;
 
    # initExtra = "
    #     ZSH_HIGHLIGHT_HIGHLIGHTERS+=(brackets pattern cursor)
    #     ";
   
-    syntaxHighlightingModule = {
+   # syntaxHighlightingModule = {
+   #   enable = true;
+   #   styles = 
+   #   "ZSH_HIGHLIGHT_HIGHLIGHTERS+=(brackets pattern cursor)";
+   #    };
+   programs.zsh.oh-my-zsh = {
       enable = true;
-      styles = {
-      "ZSH_HIGHLIGHT_HIGHLIGHTERS+=(brackets pattern cursor)";
-      }
-
-    };
-    
-    #ohmyzsh = { 
-     #enable = true;
-     # extraConfig = "
-     # ZSH_THEME = random
-     #   ";
-    #};
+      plugins = "oh-my-matrix";
+      theme = "ZSH_THEME = random";
+   } ;
+   # ohMyZsh = { 
+   #  enable = true;
+   #   theme = "ZSH_THEME = random";
+   # };
 
     shellAliases = {
       _ = "sudo";
@@ -32,15 +42,11 @@
       hg = "history | grep ";
       n = "sudo nixos-rebuild switch --flake MasterFlake/#";
       clrgenhist = "sudo nix-env -p /nix/var/nix/profiles/system --delete-generations +2";
+      zsh_theme_enable = "prompt_powerlevel9k_teardown";
+      zsh_theme_disable = "prompt_powerlevel9k_setup";
     };
 
-    zsh_theme_enable = "prompt_powerlevel9k_teardown";
-
-     historyModule = {
-        expireDuplicatesFirst = true;
-        save = 100000000;
-        size = 1000000000;
-      };
+    
 
   };
 }
