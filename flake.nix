1e7c643161fbae5d4b9548f80c8a1a140f0f6502
zell992004 <zell992004@gmail.com>
2023-10-19 14:44:03 -0400

    ad work pc

--- flake.nix
+++ flake.nix
@@ -58,6 +58,23 @@
             
         ];
       };
+
+      	nixosConfigurations = {
+		work = nixpkgs.lib.nixosSystem {
+			system = "x86_64-linux";
+			specialArgs = {inherit inputs; };
+			modules = [
+				nixos-hardware.nixosModules.lenovo-thinkpad-t480
+				./configuration.nix
+				home-manager.nixosModules.home-manager
+					{
+					home-manager.useGlobalPkgs = true;
+					home-manager.useUserPackages = true;
+					home-manager.users.peter = ./home.nix;
+					}
+				];
+			};
+
      };
 
 #    devShells = withPkgsFor (system: pkgs: {
@@ -69,5 +86,6 @@
 #	});
 
   };
+};
 }
 
