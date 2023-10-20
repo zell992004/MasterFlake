41fb6de5c93290e059ade3c42ab187f7e6a8fe85
zell992004 <zell992004@gmail.com>
2023-10-20 15:54:22 -0400

    1

--- Hosts/G14/home.nix
+++ Hosts/G14/home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, home-manager, self, inputs, lib, nixpkgs, ...}:
+{ config, pkgs, home-manager, self, inputs, lib, nixpkgs, inputs, ...}:
 
 let
   #oh-my-matrix = import ../../apps/common/oh-my-matrix.nix {inherit lib;};
